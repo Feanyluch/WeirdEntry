cfe19a56d16d734ed2c0b103717a6c06f418438e
iamfeanyluch <ifeanyioluchi7@gmail.com>
2023-10-20 16:19:04 +0100

    Updated the products component

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,31 +1,16 @@
 // pages/index.tsx
-import { GetStaticProps } from 'next';
-import ProductCard from '../components/ProductCard';
-import { ProductData } from './product';
-import { useState } from 'react';
+import ProductCard from "../components/ProductCard";
+import { ProductData } from "./product";
 
 interface ProductsProps {
   products: ProductData[];
 }
 
 const Products: React.FC<ProductsProps> = ({ products }) => {
-
-  const [cartCount, setCartCount] = useState(0);
-  const [heartCount, setHeartCount] = useState(0);
-
-  const handleAddToCart = () => {
-    setCartCount(cartCount + 1);
-  };
-
-  console.log("CarCount", cartCount)
-
-  const handleAddToFavorite = () => {
-    setHeartCount(heartCount + 1);
-  };
   return (
     <div className="grid grid-cols-3 gap-8">
       {products.map((product, index) => (
-        <ProductCard key={index} product={product} onAddToCart={handleAddToCart} onAddToFavorite={handleAddToFavorite} />
+        <ProductCard key={index} product={product} />
       ))}
     </div>
   );
