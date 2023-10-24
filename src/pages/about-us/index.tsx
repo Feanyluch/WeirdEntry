667848fd12772418c80584a2979be81b8269f381
iamfeanyluch <ifeanyioluchi7@gmail.com>
2023-10-24 20:48:30 +0100

    breadcrumb updated

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -5,10 +5,16 @@ import Link from "next/link";
 import toright from "../../../public/Images/toright.svg";
 import Breadcrumb from "@/components/BreadCrumb";
 
-const index = () => {
+import { ProductData } from "@/components/product";
+
+interface HomeProps {
+  products: ProductData[]; // Make sure the interface matches the expected prop
+}
+
+const index: React.FC<HomeProps> = ({ products }) => {
   return (
     <div>
-      <Breadcrumb />
+      <Breadcrumb products={products} />
       <div className="grid grid-cols-2 h-[700px] my-14 mx-auto max-w-[1300px] ">
         <div className="bg-[#F3E3E2]">
           <h2 className="uppercase my-4 text-center text-5xl text-[#0C0C1E]">Our story</h2>
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -2,13 +2,20 @@ import Breadcrumb from '@/components/BreadCrumb';
 import Image from 'next/image';
 import React from 'react';
 
+import { ProductData } from "@/components/product";
+
 import cartempty from "../../../public/Images/cartempty.png"
 import Link from 'next/link';
 
-const NotFound  = () => {
+interface HomeProps {
+    products: ProductData[]; // Make sure the interface matches the expected prop
+  }
+
+const NotFound: React.FC<HomeProps> = ({ products }) =>{
     return (
         <div>
-            <Breadcrumb />
+            
+      <Breadcrumb products={products} />
             <div className="flex items-center justify-center flex-col">
                 <Image src={cartempty} alt="emptycart" />
                 <h2 className='uppercase text-3xl my-4'>YOUR CART IS CURRENTLY EMPTY</h2>
